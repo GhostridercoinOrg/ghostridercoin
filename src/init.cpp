e5bdff60648ca104edbe191b064a96c0e9c270ba
9019x <174914885+9019x@users.noreply.github.com>
2024-11-23 21:20:34 -0700

    gui: fix source repository and website locations

--- src/init.cpp
+++ src/init.cpp
@@ -657,8 +657,8 @@ void SetupServerArgs()
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/CynceNYC/ghostridercoin>";
-    const std::string URL_WEBSITE = "<http://ghostridercoin.org>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/ghostridercoinorg/ghostridercoin>";
+    const std::string URL_WEBSITE = "<https://ghostridercoin.org>";
 
     return CopyrightHolders(_("Copyright (C)"), 2014, COPYRIGHT_YEAR) + "\n" +
            "\n" +
